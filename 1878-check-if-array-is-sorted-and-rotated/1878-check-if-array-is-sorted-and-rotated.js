a6573e38a7e5fbeea9ca90b3498fcb61c849385d
Kanhaiya kumar <55340068+Kanhaiya8521@users.noreply.github.com>
2025-02-02 13:39:16 +0530

    Time: 16 ms (5.05%), Space: 52.8 MB (5.05%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let rIndex = 0;
+    for(let i = 0 ; i < nums.length-1; i++) {
+        if(nums[i] > nums[i+1]) rIndex = i+1;
+    }
+    if(rIndex == 0)return true;
+    let ans = [];
+    for(let i = rIndex; i < nums.length; i++) {
+        ans.push(nums[i]);
+    }
+    for(let i = 0; i < rIndex; i++) {
+        ans.push(nums[i]);
+    }
+    console.log(ans);
+    let count = 0 ;
+    for(let i = 0 ; i < ans.length -1 ; i++) {
+        if(ans[i] > ans[i+1]) count++;
+    }
+    if(count) return false;
+    return true;
+    
+};
