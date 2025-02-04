4b24c01e464e92e05addb0df150a6178890eb8ab
Kanhaiya kumar <55340068+Kanhaiya8521@users.noreply.github.com>
2025-02-04 11:02:03 +0530

    Time: 0 ms (100%), Space: 51 MB (95.92%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.js
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.js
@@ -3,29 +3,19 @@
  * @return {number}
  */
 var longestMonotonicSubarray = function(nums) {
-    
-
-     let incLength = 1, decLength = 1;
-        let maxLength = 1;
-
-        // Iterate through array comparing adjacent elements
-        for (let pos = 0; pos < nums.length - 1; pos++) {
-            if (nums[pos + 1] > nums[pos]) {
-                // If next element is larger, extend increasing sequence
-                incLength++;
-                decLength = 1;  // Reset decreasing sequence
-            } else if (nums[pos + 1] < nums[pos]) {
-                // If next element is smaller, extend decreasing sequence
-                decLength++;
-                incLength = 1;  // Reset increasing sequence
-            } else {
-                // If elements are equal, reset both sequences
-                incLength = 1;
-                decLength = 1;
+    let maxLength = 0;
+    for(let i = 0 ; i < nums.length; i++) {
+        let increaseLen = 1;
+        let decreaseLen = 1;
+        for(let j = i ; j < nums.length-1; j++) {
+            if(nums[j+1] > nums[j] && decreaseLen === 1) {
+                increaseLen++;
             }
-
-            // Update max length considering both sequences
-            maxLength = Math.max(maxLength, Math.max(incLength, decLength));
+            else if(nums[j+1] < nums[j] && increaseLen === 1) {
+                decreaseLen++;
+            } else break;
         }
-        return maxLength;
+        maxLength = Math.max(maxLength, increaseLen, decreaseLen);
+    }
+    return maxLength;
 };
