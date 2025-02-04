65ad05f6ad17fa1209bd4cffbca7dd7e37d3b08c
Kanhaiya kumar <55340068+Kanhaiya8521@users.noreply.github.com>
2025-02-04 11:29:53 +0530

    Time: 0 ms (100%), Space: 49.2 MB (16.67%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.js
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.js
@@ -6,19 +6,11 @@ var maxAscendingSum = function(nums) {
      let maxSum = 0;
      let currSum = 0;
     for(let i = 0 ; i < nums.length; i++) {
-        let increaseLen = 1;
-        let decreaseLen = 1;
          currSum = nums[i]
         for(let j = i ; j < nums.length-1; j++) {
-            if(nums[j+1] > nums[j] && decreaseLen === 1) {
-                increaseLen++;
+            if(nums[j+1] > nums[j]) {
                 currSum += nums[j+1]
-            }
-            // else if(nums[j+1] < nums[j] && increaseLen === 1) {
-            //     decreaseLen++;
-            //     currSum += nums[j+1];
-            // }
-             else break;
+            }else break;
         }
         maxSum = Math.max(maxSum, currSum);
     }
