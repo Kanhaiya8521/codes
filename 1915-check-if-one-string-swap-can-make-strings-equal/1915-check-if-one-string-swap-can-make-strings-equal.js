944ea8f85e0071f6943dd881b8d1e8c9b22dbc24
Kanhaiya kumar <55340068+Kanhaiya8521@users.noreply.github.com>
2025-02-07 00:04:41 +0530

    Time: 12 ms (7.96%), Space: 51.9 MB (10.62%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.js
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.js
@@ -4,30 +4,32 @@
  * @return {boolean}
  */
 var areAlmostEqual = function(s1, s2) {
-    let count = 0 ;
-    if(s1.length !== s2.length)return false;
+    if (s1 === s2) return true;
 
-    let str1 = "", str2 = "", str3= "", str4="";
-    for(let i = 0 ; i < s1.length; i++) {
-        if(s1[i] !== s2[i]) {
-            str1=s1[i];
-            str2=s2[i];
-            count++;
+    let s1FrequencyMap = new Array(26).fill(0);
+    let s2FrequencyMap = new Array(26).fill(0);
+    let numDiffs = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+        let s1Char = s1[i];
+        let s2Char = s2[i];
+
+        if (s1Char !== s2Char) {
+            numDiffs++;
+            if (numDiffs > 2) return false; 
         }
-        if(count === 1) {
-            str3 = str1;
-            str4 = str2;
-        }
-        if(count > 2) return false;
+        // Increment frequency counts
+        s1FrequencyMap[s1Char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        s2FrequencyMap[s2Char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    console.log(s1FrequencyMap)
+
+    // Check if frequency counts match
+    for (let i = 0; i < 26; i++) {
+        if (s1FrequencyMap[i] !== s2FrequencyMap[i]) return false;
     }
 
-    let str5 = str1;
-    str1 = str2;
-    str2 = str5;
-    console.log(str1 ,str2 , str3 ,str4)
-
-    if(str1 === str3 && str2 === str4) return true;
-    
-    return false;
+    return true;
     
 };
+
