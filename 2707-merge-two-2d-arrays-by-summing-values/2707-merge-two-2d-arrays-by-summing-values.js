3e63f9ff04cbcf83a2f07881ac9056187a28f037
Kanhaiya kumar <55340068+Kanhaiya8521@users.noreply.github.com>
2025-03-03 00:22:34 +0530

    Time: 6 ms (43.42%), Space: 60.1 MB (10.53%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.js
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.js
@@ -4,25 +4,37 @@
  * @return {number[][]}
  */
 var mergeArrays = function(nums1, nums2) {
-    let map = new Map();
-    for(let num of nums1) {
-        map.set(num[0], num[1]);
-    }
-    console.log(map)
-
-    for(let num of nums2) {
-        if(map.get(num[0])) {
-          map.set(num[0], map.get(num[0])+num[1]);
-
-        } else
-        map.set(num[0], num[1]);
-    }
-    const sortedByKey = new Map([...map.entries()].sort((a, b) => a[0] - b[0]));
+    let i = 0, j = 0;
     let ans = [];
-    for (const [key, value] of sortedByKey) {
-        ans.push([key, value])
-  console.log(`Key: ${key}, Value: ${value}`);
-}
-    return ans
+
+    while(i < nums1.length && j < nums2.length){
+        const [id1, val1] = nums1[i];
+        const [id2, val2] = nums2[j];
+
+        if(id1 > id2) {
+            ans.push([id2, val2]);
+            j++;
+        } else if(id1 < id2) {
+            ans.push([id1, val1]);
+            i++;
+        } else {
+            ans.push([id1, val1 + val2]);
+            i++;
+            j++;
+        }
+    }
+
+    while(i < nums1.length) {
+        const [id1, val1] = nums1[i];
+        ans.push([id1, val1]);
+        i++;
+    }
+    while(j < nums2.length) {
+        const [id1, val1] = nums2[j];
+        ans.push([id1, val1]);
+        j++;
+    }
+
+    return ans;
     
 };
